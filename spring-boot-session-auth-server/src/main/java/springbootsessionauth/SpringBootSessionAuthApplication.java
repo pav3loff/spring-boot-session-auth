d9aaf999251c41349cc87c5f741798732a59c246
Mario Pavlic <886pav@gmail.com>
2021-02-08 19:49:49 +0100

    init structure

---
+++ spring-boot-session-auth-server/src/main/java/springbootsessionauth/SpringBootSessionAuthApplication.java
@@ -0,0 +1,13 @@
+package springbootsessionauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSessionAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSessionAuthApplication.class, args);
+	}
+
+}
---
+++ spring-boot-session-auth-server/src/test/java/springbootsessionauth/SpringBootSessionAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootsessionauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSessionAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
